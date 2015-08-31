98be6aa57911eca835b7acc1b091257f24ec412f
Shaojung <shaojung@gmail.com>
2015-08-31 11:55:23 +0800

    First Commit.

---
+++ src/edu/pccu/order/Order.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.pccu.order;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Order {
+    int id;
+    String CustomerName;
+    String CustomerTel;
+    ArrayList<OrderDetail> Detail;
+}
---
+++ src/edu/pccu/order/OrderDetail.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.pccu.order;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class OrderDetail {
+    int id;
+    String ItemName;
+    int num;
+}
